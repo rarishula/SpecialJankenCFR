08b747f719af7e58f6ec7938b8dd0f2e27bdf813
rarishula <67272393+rarishula@users.noreply.github.com>
2024-01-22 05:33:56 +0900

    Update delete test strategy

--- CFR.py
+++ CFR.py
@@ -92,10 +92,9 @@ class CFR:
                 cumulative_regrets[state] = [0] * self.num_actions
             cumulative_regrets[state][actual_action] += current_regret
 
-        """
+
 
         def update_strategy(self, player, state):
-                original_strategy = self.player2_strategy_profile.copy()  # コピーを作成
                 
                 # プレイヤーに応じた累積後悔値を取得
                 cumulative_regrets = self.player1_cumulative_regrets if player == 1 else self.player2_cumulative_regrets
@@ -121,23 +120,7 @@ class CFR:
                 print(f"State: {state}, Player: {player}, Cumulative Regrets: {regrets}, Positive Regret Sum: {positive_regret_sum}")
         
                 return new_strategy
-        """
 
-        def update_strategy(self, player, state):
-            # プレイヤーに応じた戦略プロファイルを選択
-            if player == 1:
-                strategy_profile = self.player1_strategy_profile
-            else:
-                strategy_profile = self.player2_strategy_profile
-        
-            # その状態の戦略が未定義の場合、均等な確率で初期化
-            if state not in strategy_profile:
-                strategy_profile[state] = [1.0 / self.num_actions] * self.num_actions
-        
-            # 戦略を更新
-            strategy_profile[state] = strategy_profile[state]
-        
-            return strategy_profile[state]
 
         
         def train(self, num_iterations):
