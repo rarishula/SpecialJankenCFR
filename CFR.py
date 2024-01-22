865ddde8bfe284084720a5f9c1ed7b70ca6fad76
rarishula <67272393+rarishula@users.noreply.github.com>
2024-01-22 21:49:45 +0900

    Update CFR.py

--- CFR.py
+++ CFR.py
@@ -196,5 +196,5 @@ player2_initial_strategy = [1/3, 1/3, 1/3]
 
 # CFRインスタンスの作成とトレーニングの実行
 cfr = CFR(player1_initial_strategy, player2_initial_strategy)
-cfr.train(10)  # 1000反復でトレーニング
+cfr.train(1000)  # 1000反復でトレーニング
 
